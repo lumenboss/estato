a2c19cb0c6bf5478b8010a7b39a2b4a03c408835
lumen nganje <lumennganje592@gmail.com>
2024-10-05 13:26:51 +0100

    connection to mongodb done

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=> console.log("Successful connection to mongodb")
+).catch((err)=> console.log(err));
 
 const app = express()
 
-
-app.listen(3000, () => console.log("Running form the server"));
+app.listen(3000, () => console.log("express server running on port 3000"));
+// Q1EyAfrtbePODgsK
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <header className="py-2 px-2 sm:px-6 bg-blue-400 flex justify-between items-center sm:gap-3 max-sm:flex-col">
+    <header className="py-2 px-2 sm:px-6 md:px-8 lg:px-12 bg-blue-400 flex justify-between items-center sm:gap-3 max-sm:flex-col">
         <span className=" py-1 italic text-white text-2xl font-serif sm:text-3xl">Estato</span>
         <nav className="flex items-center gap-1 sm:gap-4 max-sm:">
             <form className="h-8 max-w-max p-1 rounded-sm bg-white flex items-center">
-                <input type="search" placeholder="Search" className="p-1 w-40 sm:w-48 focus:outline-none"/>
+                <input type="search" placeholder="Search" className="p-1 w-40 sm:w-48 md:w-72 focus:outline-none"/>
                 <FaSearch />
             </form>
             <ul className="flex items-center gap-3 sm:gap-3 md:gap-5">
